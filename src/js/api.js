faacfb9d507066371338df036fa45734b28d15a3
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-02 15:46:34 -0300

    Updates api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,18 +1,15 @@
-function handleResponse(response) {
-  if (response.ok) {
-    return response.json();
-  }
-  const error = {
-    status: response.status,
-    statusText: response.statusText,
-    image: '/src/img/worried-pikachu.png',
-    message: 'Sorry, something went wrong! Try again in a few minutes!.',
-  };
-  return Promise.reject(error);
-}
-
 export default async function getPokemon(id) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-  const responseJSON = await handleResponse(response);
-  return responseJSON;
+
+  if (!response.ok) {
+    const error = new Error();
+
+    error.image = '/src/img/worried-pikachu.png';
+    error.message = 'Sorry, something went wrong! Try again in a few minutes!.';
+
+    throw error;
+  }
+
+  const pokemonData = await response.json();
+  return pokemonData;
 }
