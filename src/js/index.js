a659cade8d0b06306031bafcd23944cbe4387b67
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-02 15:51:13 -0300

    Updates index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
-import {
-  showFirstPokemon,
-  showNewPokemon,
-} from './services.js';
+import init from './pokedex.js';
 
-import { closeModal } from './ui.js';
-
-showFirstPokemon();
-document.querySelector('#next').onclick = showNewPokemon;
-document.querySelector('#back').onclick = showNewPokemon;
-document.querySelector('#search').onclick = showNewPokemon;
-document.querySelector('#close-modal').onclick = closeModal;
+init();
