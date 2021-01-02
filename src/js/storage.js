9f15027db89c75d3191784eca3fab0712df180f2
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-02 15:48:20 -0300

    Updates storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,15 +1,18 @@
-export function getPokemonFromStorage(id) {
+export function getPokemon(id) {
   const pokemon = JSON.parse(localStorage.getItem(id));
-  if (pokemon === null) {
+
+  if (!pokemon) {
     throw new Error('Pokemon not in storage.');
   }
+
   return pokemon;
 }
 
-export function savePokemonInStorage(pokemon) {
-  if (pokemon === undefined) {
-    throw new Error('Pokemon is undefined');
+export function savePokemon(pokemon) {
+  if (!pokemon) {
+    throw new Error('A pokemon is needed in order to save it.');
   }
+
   try {
     localStorage.setItem(`${pokemon.id}`, JSON.stringify(pokemon));
   } catch (error) {
