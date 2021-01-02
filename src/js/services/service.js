99dabbe7f5f0f6406a9ff450c3d09eeef2ed897b
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-02 16:19:07 -0300

    Removes repository

--- src/js/services/service.js
+++ src/js/services/service.js
@@ -1,6 +1,19 @@
-import getPokemonFromRepository from '../repository/repository.js';
+import getPokemonFromAPI from '../api/api.js';
+import {
+  getPokemon as getPokemonFromStorage,
+  savePokemon as savePokemonInStorage,
+} from '../storage/storage.js';
+import pokemonMapper from '../mappers/mapper.js';
 
 export default async function getPokemonById(pokemonId) {
-  const pokemon = await getPokemonFromRepository(pokemonId);
-  return pokemon;
+  try {
+    const pokemonData = getPokemonFromStorage(pokemonId);
+    const pokemon = pokemonMapper(pokemonData);
+    return pokemon;
+  } catch (error) {
+    const pokemonData = await getPokemonFromAPI(pokemonId);
+    savePokemonInStorage(pokemonData);
+    const pokemon = pokemonMapper(pokemonData);
+    return pokemon;
+  }
 }
