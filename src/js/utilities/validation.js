2440da348d1c10613206d71fb6759d9b7eb9c42f
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-04 22:15:11 -0300

    Updates paths

--- src/js/utilities/validation.js
+++ src/js/utilities/validation.js
@@ -6,7 +6,7 @@ export default function validateInput(input) {
   const error = new Error();
 
   if (input.length === 0) {
-    error.image = '/src/img/pikachu-pickaxe.jpg';
+    error.image = '/img/pikachu-pickaxe.jpg';
     error.message = "Don't mess with the search bar!";
 
     throw error;
