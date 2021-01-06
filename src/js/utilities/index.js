26fa728f64658a7c46a80933c6823fdf66f1cc55
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-06 18:24:46 -0300

    Removes unused function fix
    Forgot to update a line after the removal.

--- src/js/utilities/index.js
+++ src/js/utilities/index.js
@@ -1,4 +1,4 @@
-import { getNewPokemonId, setCurrentPokemonId } from './pagination.js';
+import getNewPokemonId from './pagination.js';
 import validateInput from './validation.js';
 
-export { getNewPokemonId, setCurrentPokemonId, validateInput };
+export { getNewPokemonId, validateInput };
